a7112761e8847fbc0ff62af3cae7dd253e472ee0
Jakob Willforss <jakob.willforss@hotmail.com>
2021-06-21 22:06:02 +0200

    Setup minimum amplify login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+Amplify.configure(awsconfig);
+
+// Use this as a starting point:
+// https://www.freecodecamp.org/news/ultimate-guide-to-aws-amplify-and-reacxt/
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
+        <h2>Tip of the Brain!</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
